ade9e866794ce270e4f73d9c7cc4570d540c4a34
lbtsm <1425930815@qq.com>
2023-05-04 10:14:45 +0800

    fix tx complete logic

--- pkg/monitor/struct.go
+++ pkg/monitor/struct.go
@@ -3,9 +3,10 @@ package monitor
 import "time"
 
 type BridgeTransactionInfo struct {
-	Id            int         `gorm:"column:id" db:"id" json:"id" form:"id"`
-	SourceChainId interface{} `gorm:"column:source_chain_id" db:"source_chain_id" json:"source_chain_id" form:"source_chain_id"`
-	SourceHash    interface{} `gorm:"column:source_hash" db:"source_hash" json:"source_hash" form:"source_hash"`
-	CompleteTime  *time.Time  `gorm:"column:complete_time" db:"complete_time" json:"complete_time" form:"complete_time"`
-	Timestamp     *time.Time  `gorm:"column:timestamp" db:"timestamp" json:"timestamp" form:"timestamp"`
+	Id              int         `gorm:"column:id" db:"id" json:"id" form:"id"`
+	SourceChainId   interface{} `gorm:"column:source_chain_id" db:"source_chain_id" json:"source_chain_id" form:"source_chain_id"`
+	SourceHash      interface{} `gorm:"column:source_hash" db:"source_hash" json:"source_hash" form:"source_hash"`
+	DestinationHash interface{} `gorm:"column:destination_hash" db:"destination_hash" json:"destination_hash" form:"destination_hash"`
+	CompleteTime    *time.Time  `gorm:"column:complete_time" db:"complete_time" json:"complete_time" form:"complete_time"`
+	Timestamp       *time.Time  `gorm:"column:timestamp" db:"timestamp" json:"timestamp" form:"timestamp"`
 }
